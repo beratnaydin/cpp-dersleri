866dbf29719a29f229d11f9e8a28a6850f71c005
Berat Nihat Aydın <beratnaydin@outlook.com>
2025-07-12 22:50:28 +0300

    Create cpp_ders1.cpp
    ders1: değişkenler ve cout örnekleri eklendi

---
+++ cpp_ders1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+int yas =20 ;
+float boy = 1.75;
+char cinsiyet = 'E';
+cout<<"Benim ismim Berat"<<endl;
+cout<<"Yasim"<<"="<<yas<<endl;
+cout<<"cinsiyetim"<<"="<<cinsiyet<<endl;
+	
+return 0;		
+}
+
+
